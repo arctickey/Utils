0318470c728859949abf95f928ade7a8a96a4102
Filip Chrzuszcz <filipchrzuszcz1@gmail.com>
2021-10-17 10:40:21 +0200

    Fix returned types from all functions

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup
-setup(name='utils',
-      version='0.1',
-      packages = ['utils']
-      )
+
+setup(name="utils", version="0.1", packages=["utils"])
--- utils/connect.py
+++ utils/connect.py
@@ -1,34 +1,38 @@
 from pymongo import MongoClient
 
-def connect(host='localhost', port=27017):
-    '''Connect to MongoDB'''
+
+def connect(host="localhost", port=27017):
+    """Connect to MongoDB"""
     return MongoClient(host, port)
 
-def is_id(x):
-    return not x[0] == '_id'
 
-def read_mongo(db :str = 'ZMS', collection: str='profile',no_id:bool = False):
-    """ Read from Mongo and Store into DataFrame """
+def is_id(x):
+    return not x[0] == "_id"
+
+
+def read_mongo(db: str = "ZMS", collection: str = "profile", no_id: bool = False):
+    """Read from Mongo and Store into DataFrame"""
     conn = connect()
     db = conn[db]
     collection = db[collection]
     df = list(collection.find())
     if no_id:
-        df = [dict(filter(is_id,list(x.items()))) for x in df]
+        df = [dict(filter(is_id, list(x.items()))) for x in df]
     return df
 
 
-def save_mongo(df: dict, db :str = 'ZMS', collection : str ='profile'):
-    '''Read from dict or DataFrame and saves to MongoDB'''
+def save_mongo(df: dict, db: str = "ZMS", collection: str = "profile"):
+    """Read from dict and saves to MongoDB"""
     conn = connect()
     db = conn[db]
     collection = db[collection]
     collection.insert_many(df)
     return True
 
-def drop_duplicates(df:list) -> list:
-    '''Drop duplicates from list of dictionaries'''
-    df = [dict(filter(is_id,list(x.items()))) for x in df]
+
+def drop_duplicates(df: list) -> list:
+    """Drop duplicates from list of dictionaries"""
+    df = [dict(filter(is_id, list(x.items()))) for x in df]
     no_dups = []
     for x in df:
         if x not in no_dups:
