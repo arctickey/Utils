b273833b84c47b89ea77fd5b0682741bdde48a8a
Filip Chrzuszcz <42840114+arctickey@users.noreply.github.com>
2021-11-07 12:24:54 +0100

    Update kit.py

--- utils/kit.py
+++ utils/kit.py
@@ -1,12 +1,12 @@
 def insert_kit(db, kit):
     """Inserts kit to the kits' collection.
     :param db: database connection to client.ZMS
-    :param kit: dictionary in schema {"Name": "abc", "Allels": ["a", "b", "c"]}
+    :param kit: dictionary in schema {"name": "abc", "Allels": ["a", "b", "c"]}
     """
     # Check for Name or Allels duplicate
-    if db["kits"].count_documents({"Name": kit["Name"]}, limit=1) != 0:
+    if db["kits"].count_documents({"name": kit["name"]}, limit=1) != 0:
         raise Exception("Kit with specified name already exists.")
-    elif db["kits"].count_documents({"Allels": kit["Allels"]}, limit=1) != 0:
+    elif db["kits"].count_documents({"allels": kit["allels"]}, limit=1) != 0:
         raise Exception("Kit with specified allels' marker already exists.")
     # Insert
     db["kits"].insert_one(kit)
