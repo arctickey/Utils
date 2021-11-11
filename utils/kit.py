593a7f23809dc610fd4020c91ff93eca8fbb5890
Piotr Fic <43789503+okcze@users.noreply.github.com>
2021-11-11 16:59:49 +0100

    add load for kits

--- utils/kit.py
+++ utils/kit.py
@@ -1,4 +1,4 @@
-def insert_kit(db, kit):
+def insert_kit(db: str, kit: str):
     """Inserts kit to the kits' collection.
     :param db: database connection to client.ZMS
     :param kit: dictionary in schema {"Name": "abc", "Allels": ["a", "b", "c"]}
@@ -7,6 +7,19 @@ def insert_kit(db, kit):
     if db["kits"].count_documents({"Name": kit["Name"]}, limit=1) != 0:
         raise Exception("Kit with specified name already exists.")
     elif db["kits"].count_documents({"Allels": kit["Allels"]}, limit=1) != 0:
-        raise Exception("Kit with specified allels' marker already exists.")
+        raise Exception("Kit with specified allels' markers already exists.")
     # Insert
     db["kits"].insert_one(kit)
+
+
+def load_kit(db: str, kit_name: str):
+    """Loads kit from the kits' collection.
+    :param db: database connection to client.ZMS
+    :param kit_name: name of the kit passed during inserting
+    """
+    # try:
+    kit = db["kits"].find({"Name": kit_name})
+    # except:
+    #     raise Exception("Kit with specified name already exists.")
+
+    return kit
