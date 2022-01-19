74c9715a07065abc347149197c8c97d0d88bf0ec
Piotr Fic <43789503+okcze@users.noreply.github.com>
2022-01-19 20:43:28 +0100

    Update read_txt.py

--- utils/read_txt.py
+++ utils/read_txt.py
@@ -5,12 +5,12 @@ from utils.upload_helpers import *
 def read_txt(path):
     df = pd.read_table(path)
     df = df.loc[:, df.columns[:-1]]
-    df.columns = ["Sample_Name", "Panel", "Marker", "Allel_1", "Allel_2"]
-    df["Allel"] = list(zip(df["Allel_1"], df["Allel_2"]))
-    df.drop(["Allel_1", "Allel_2", "Panel"], axis=1, inplace=True)
-    df_wide = df.pivot(index="Sample_Name", columns="Marker", values="Allel")
+    # df.columns = ["Sample_Name", "Panel", "Marker", "Allel_1", "Allel_2"]
+    df["Allel"] = [[j for j in df.iloc[i, 3:]] for i in range(df.shape[0])]
+    # df.drop(["Allel_1", "Allel_2", "Panel"], axis=1, inplace=True)
+    df_wide = df.pivot(index="Sample Name", columns="Marker", values="Allel")
     df_wide.reset_index(inplace=True, drop=False)
-    df_wide.rename({"Sample_Name": "opinion"}, axis=1, inplace=True)
+    df_wide.rename({"Sample Name": "opinion"}, axis=1, inplace=True)
     to_delete = ["K+", "ladder"]
     df_wide = df_wide.loc[~df_wide["opinion"].isin(to_delete)]
     df_wide = df_wide.to_dict(orient="records")
